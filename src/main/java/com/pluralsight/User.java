02e790c395ec2c9ec3d2ed9100b1a363a516693e
Ahsan <sbaseer25@gmail.com>
2025-10-15 15:28:45 -0400

    added comments for User class

--- src/main/java/com/pluralsight/User.java
+++ src/main/java/com/pluralsight/User.java
@@ -1,11 +1,15 @@
 package com.pluralsight;
 
+//created a user class to represent a user that uses the ledger, is associated with a transaction, and has
+//different permissions for access.
 public class User {
     private int id;
     private String name;
-    private String pin;          // now a String (preserves leading zeros)
-    private boolean adminAccess; // stays boolean
+    private String pin; // used a String for PIN since
+    private boolean adminAccess; // this represents if the user is regular user or superuser
 
+    //User obj constructor sets everything but the admin access.
+    //This way every new user would by default be a regular user
     public User(int id, String name, String pin) {
         this.id = id;
         this.name = name;
@@ -13,22 +17,37 @@ public class User {
         this.adminAccess = false;
     }
 
-    public User(int id, String name, String pin, boolean adminAccess) {
-        this.id = id;
-        this.name = name;
-        this.pin = pin;
-        this.adminAccess = adminAccess;
+    //Getters and Setters for member fields
+    public int getId() {
+        return id;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getPin() { return pin; }
-    public void setPin(String pin) { this.pin = pin; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public boolean isAdminAccess() { return adminAccess; }
-    public void setAdminAccess(boolean adminAccess) { this.adminAccess = adminAccess; }
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public boolean isAdminAccess() {
+        return adminAccess;
+    }
+
+    //this will be used to set access permissions and make a user an Admin
+    public void setAdminAccess(boolean adminAccess) {
+        this.adminAccess = adminAccess;
+    }
 }
